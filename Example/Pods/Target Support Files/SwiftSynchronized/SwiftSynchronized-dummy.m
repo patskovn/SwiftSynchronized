a10074962d56fc80799c5ecc27759017e4831bc7
nd.packov <nd.packov@tensor.ru>
2018-02-22 09:36:59 +0300

    Add project

---
+++ Example/Pods/Target Support Files/SwiftSynchronized/SwiftSynchronized-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SwiftSynchronized : NSObject
+@end
+@implementation PodsDummy_SwiftSynchronized
+@end
---
+++ Example/SwiftSynchronized/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SwiftSynchronized
+//
+//  Created by nd.packov on 02/22/2018.
+//  Copyright (c) 2018 nd.packov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
